b69e020c1d50dff716d8633029756e12a3c23bdc
Om Telrandhe <omtelrandhe30@gmail.com>
2024-05-25 19:36:15 +0530

    first commit

---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import image from '../assets/NEWS.png'
+
+const NewsItem = ({title, description, src, url}) => {
+  return (
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth: "345px"}}>
+  <img src={src?src:image} className="card-img-top" alt="..." style={{height: "200px",width:"328px"}}/>
+  <div className="card-body">
+    <h5 className="card-title">{title.slice(0,50)}</h5>
+    <p className="card-text">{description?description.slice(0,90):"News is about the current event. It is an information about something that has happened recently."}</p>
+    <a href={url} className="btn btn-primary">Read More</a>
+  </div>
+</div>
+  )
+}
+
+export default NewsItem
